4d99edeec1bcf0d8b395372d509cb6aec2f5f098
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-05-19 14:07:06 +0530

    Feature Update
    Added:
    
    - Box colour picker
    - Informational content

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 const DEFAULT_GRID_SIZE = 16;
 const MAX_GRID_SIZE = 100;
-const MAX_PX_PER_BOX = 30; // in px for e.g. 30px;
-const BOX_COLOUR_ON_HOVER = "#000000";
-const CTRL_PRESS_LISTEN = false;
+const MAX_PX_PER_BOX = 30; // in px foe e.g. 30px;
+const DEFAULT_MULTI_COLOUR = false;
+const DEFAULT_CTRL_PRESS_LISTEN = true;
+const BOX_COLOUR_ON_HOVER = "#00b4d8";
 
 const sketchpad = document.querySelector(".sketchpad");
-const sizeButton = document.querySelector("#inputSize");
+const sizeChangeForm = document.querySelector("#sizeChangeForm");
+const sizeInput = document.querySelector("#inputSize");
+const colourPicker = document.querySelector("#colourPicker");
+const multiColor = document.querySelector("#multiColourSwitch");
 const resetButton = document.querySelector("#resetGrid");
-const ctrlCheckbox = document.querySelector("#ctrlOnly");
+const ctrlCheckbox = document.querySelector("#ctrlOnlySwitch");
 
 function makeGrid(size) {
   Array.from(sketchpad.children).forEach((element) => element.remove());
@@ -30,8 +34,12 @@ function makeGrid(size) {
 
       box.addEventListener("mouseover", () => {
         if (ctrlPressListen) {
-          if (ctrlPressed) box.style.background = BOX_COLOUR_ON_HOVER;
-        } else box.style.background = BOX_COLOUR_ON_HOVER;
+          if (ctrlPressed) box.style.background = boxColourHover;
+        } else box.style.background = boxColourHover;
+      });
+
+      box.addEventListener("click", () => {
+        box.style.background = boxColourHover;
       });
 
       row.append(box);
@@ -40,39 +48,50 @@ function makeGrid(size) {
   }
 }
 
+// reset the grid: set all squares to white
 function resetGrid() {
   if (sizeUser == undefined) sizeUser = DEFAULT_GRID_SIZE;
   makeGrid(sizeUser);
 }
-
 resetButton.addEventListener("click", () => resetGrid());
 document.addEventListener("keydown", (event) => {
   if (event.key == "r" || event.key == "R") resetGrid();
 });
 
+function isTrue(value) {
+  return value === "true";
+}
+
+function sizeError(feedback) {
+  sizeUser = DEFAULT_GRID_SIZE;
+  alert(feedback);
+  sizeInput.value = "";
+}
+
 // modify grid size based on user input
 let sizeUser;
-sizeButton.addEventListener("click", () => {
-  try {
-    sizeUser = prompt(
-      "Enter the number of squares per side for the grid:"
-    ).replace(/ /g, "");
-  } catch (TypeError) {}
-  if (sizeUser == null || sizeUser == "" || isNaN(sizeUser)) {
-    sizeUser = DEFAULT_GRID_SIZE;
-    alert(
-      "Please enter a valid size. For example, to obtain a 16 by 16 grid, enter 16"
+sizeChangeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sizeUser = sizeInput.value.replace(/ /g, "");
+  if (sizeUser == "" || isNaN(sizeUser) || sizeUser < 1)
+    sizeError(
+      "Please enter a valid size. For example, to obtain a 32 by 32 grid, enter 32"
     );
-  } else if (sizeUser > MAX_GRID_SIZE) {
-    sizeUser = DEFAULT_GRID_SIZE;
-    alert(`Please enter a smaller size, maximum is ${MAX_GRID_SIZE}.`);
-  } else makeGrid(sizeUser);
+  else if (sizeUser > MAX_GRID_SIZE)
+    sizeError(`Please enter a smaller size, maximum is ${MAX_GRID_SIZE}.`);
+  else makeGrid(sizeUser);
 });
 
 // listen for control key checkbox change
-let ctrlPressListen = CTRL_PRESS_LISTEN;
-document.querySelector("input[type='checkbox']").checked = true;
+let localStorageCtrlPressListen = localStorage.getItem("ctrlOnlySwitch");
+
+if (localStorageCtrlPressListen !== null)
+  ctrlCheckbox.checked = isTrue(localStorageCtrlPressListen);
+else ctrlCheckbox.checked = DEFAULT_CTRL_PRESS_LISTEN;
+let ctrlPressListen = ctrlCheckbox.checked;
+
 ctrlCheckbox.addEventListener("change", (event) => {
+  localStorage.setItem("ctrlOnlySwitch", event.target.checked);
   if (event.target.checked) ctrlPressListen = true;
   else ctrlPressListen = false;
 });
@@ -85,5 +104,44 @@ document.addEventListener("keydown", (event) => {
 document.addEventListener("keyup", (event) => {
   if (event.key == "Control") ctrlPressed = false;
 });
+sketchpad.addEventListener("mouseleave", () => {
+  ctrlPressed = false;
+});
 
+// colour picker stuff
+let localStorageBoxColor = localStorage.getItem("boxColor");
+if (localStorageBoxColor !== null) colourPicker.value = localStorageBoxColor;
+else colourPicker.value = BOX_COLOUR_ON_HOVER;
+
+let boxColourHover = colourPicker.value;
+
+let localStorageMultiColor = isTrue(localStorage.getItem("multiColor"));
+if (localStorageMultiColor !== null)
+  multiColor.checked = localStorageMultiColor;
+else multiColor.checked = DEFAULT_MULTI_COLOUR;
+let multiColorEnabled = multiColor.checked;
+
+multiColor.addEventListener("click", () =>
+  localStorage.setItem("multiColor", multiColor.checked)
+);
+Coloris({
+  alpha: false,
+  swatches: [
+    "#2a9d8f",
+    "#e9c46a",
+    "rgb(244,162,97)",
+    "#d62828",
+    "#0096c7",
+    "#00b4d880",
+  ],
+  onChange: (colour) => {
+    localStorage.setItem("boxColor", colour);
+    localStorage.setItem("multiColor", multiColor.checked);
+    multiColorEnabled = multiColor.checked;
+    if (!multiColorEnabled) resetGrid();
+    boxColourHover = colour;
+  },
+});
+
+// initialize grid at page load
 makeGrid(DEFAULT_GRID_SIZE);
